affc9d5b5aff330b3deb508dcd766e5430ed69fb
Thiago Luis <thiagoluisrpinho@gmail.com>
2017-12-07 20:06:48 -0200

    Preço da refeição mudado de Inteiro para Real

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171207210911) do
+ActiveRecord::Schema.define(version: 20171207220341) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,7 +18,7 @@ ActiveRecord::Schema.define(version: 20171207210911) do
   create_table "meals", force: :cascade do |t|
     t.string   "title"
     t.text     "description"
-    t.integer  "price"
+    t.float    "price"
     t.integer  "category"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
