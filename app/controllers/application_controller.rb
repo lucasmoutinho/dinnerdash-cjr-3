ea5f9130f7b4de7590976c6b12f7dd5e065f01b6
JLYamin <erwn36@gmail.com>
2017-12-07 16:49:13 -0200

    Adicionado link no topo das páginas e adicionado atributo username no User

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,15 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+#adicionado seguindo o howto na wiki do deviser sobre username/email
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    added_attrs = [:username, :email, :password, :password_confirmation, :remember_me]
+    devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
+    devise_parameter_sanitizer.permit :account_update, keys: added_attrs
+  end
+
 end
