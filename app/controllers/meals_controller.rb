8d1288cffd8e88d636a480ce70f45f711e0ae944
Thiago Luis <thiagoluisrpinho@gmail.com>
2017-12-07 19:16:14 -0200

    Adicionado controller Meals, model Meal, Create e Show do controller Meals

---
+++ app/controllers/meals_controller.rb
@@ -0,0 +1,26 @@
+class MealsController < ApplicationController
+	def index
+	  @meals = Meal.all
+  	end	
+
+
+	def show
+    @meal = meal.find(params[:id])
+  	end
+
+
+	def new
+	end
+
+	def create
+	  @meal = Meal.new(meal_params)
+	 
+	  @meal.save
+	  redirect_to @meal
+	end
+ 
+private
+  def meal_params
+    params.require(:meal).permit(:title, :description, :price, :category)
+  end
+end
