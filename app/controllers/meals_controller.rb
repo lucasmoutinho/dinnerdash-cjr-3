d4b64c7cbf701880e02f85ffbdce7a396dcc1ec2
Thiago Luis <thiagoluisrpinho@gmail.com>
2017-12-07 19:47:53 -0200

    New e Index funcionam

--- app/controllers/meals_controller.rb
+++ app/controllers/meals_controller.rb
@@ -10,14 +10,40 @@ class MealsController < ApplicationController
 
 
 	def new
-		 @meal = Meal.new
+		@meal = Meal.new
 	end
 
 	def create
-	  @meal = Meal.new(meal_params)
+	  	puts "vim ate o create!!!!"
+
+		@meal = Meal.new(meal_params)
+		if @meal.save
+			redirect_to meals_path, notice: "Refeição cadastrada com sucesso!!"
+		else
+			redirect_to new_meal_path, notice: "Refeição Não pode ser salvo!"
+		end
+	end
+
+		def destroy
+		@meal = Meal.find(params[:id])
+		@meal.delete
+
+		redirect_to meals_path, notice: "Refeição foi apagada"
+	end
+
+	def edit
+		@meal = Meal.find(params[:id])
+	end
+
+
+	def update
+	  @meal = Meal.find(params[:id])
 	 
-	  @meal.save
-	  redirect_to @meal
+	  if @meal.update(meal_params)
+	    redirect_to @meal
+	  else
+	    render 'edit'
+	  end
 	end
  
 private
