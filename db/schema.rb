8709f06bf3dc9954d0c478ba2c04230a15a2ee92
JLYamin <erwn36@gmail.com>
2017-12-11 22:33:51 -0200

    Feita view do Perfil. Header e Footer ajeitados para todas as páginas (com exceção da opção backoffice no header)

--- db/schema.rb
+++ db/schema.rb
@@ -15,16 +15,15 @@ ActiveRecord::Schema.define(version: 20171210143706) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "meals", force: :cascade do |t|
-    t.string   "title"
-    t.text     "description"
-    t.float    "price"
-    t.integer  "category"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
-    
+  create_table "meals", id: :serial, force: :cascade do |t|
+    t.string "title"
+    t.text "description"
+    t.float "price"
+    t.integer "category"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
-  
+
   create_table "users", id: :serial, force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
