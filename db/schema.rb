1c096dec043d4288e6ace33a1bb4433efd78cdc6
Lucas Moutinho <lucas.silvamoutinho@gmail.com>
2017-12-17 16:22:25 -0200

    migrations

--- db/schema.rb
+++ db/schema.rb
@@ -27,8 +27,8 @@ ActiveRecord::Schema.define(version: 20171217000717) do
     t.float "price"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "picture"
     t.bigint "meal_category_id"
+    t.string "picture"
     t.boolean "available"
     t.index ["meal_category_id"], name: "index_meals_on_meal_category_id"
   end
@@ -53,7 +53,6 @@ ActiveRecord::Schema.define(version: 20171217000717) do
     t.datetime "updated_at", null: false
     t.string "username"
     t.boolean "admin", default: false
-    t.string "image_id"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
     t.index ["username"], name: "index_users_on_username", unique: true
