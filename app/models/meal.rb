8557af8668bc34bd10f1ae01a4eeb8cc12cae0b5
Thiago Luis <thiagoluisrpinho@gmail.com>
2017-12-19 14:00:41 -0200

    Alterações nos models Cart, Meal e criado colunas para Cart_items

--- app/models/meal.rb
+++ app/models/meal.rb
@@ -2,6 +2,8 @@ class Meal < ApplicationRecord
 
 	mount_uploader :picture, PictureUploader
 	belongs_to :meal_category
+	has_many :cart_items
+	has_many :carts, :through => :cart_items
 
 end
 	
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171219154254) do
+ActiveRecord::Schema.define(version: 20171219155140) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "cart_items", force: :cascade do |t|
+    t.integer "meal_id"
+    t.integer "cart_id"
+    t.float "price"
+    t.integer "quantity"
+    t.datetime "created_at"
+  end
 
   create_table "carts", force: :cascade do |t|
     t.datetime "created_at", null: false
